85e8a60bf16da138c51d6e0c4f95e8758ecefdee
Muhammed Siju <msiju@codeaurora.org>
2015-03-18 18:20:13 +0530

    Telephony MSIM: Change subid from long to int.
    Change subid from long to int for getActiveSubscription()
    and switchToOtherActiveSub() APIs.
    
    Change-Id: I6db643eb1a5dd7ae1bf6f8df72579c47f2096a2a
    CRs-Fixed: 810354

--- telecomm/java/android/telecom/TelecomManager.java
+++ telecomm/java/android/telecom/TelecomManager.java
@@ -931,7 +931,7 @@ public class TelecomManager {
      * Returns current active subscription.
      * @hide
      */
-    public long getActiveSubscription() {
+    public int getActiveSubscription() {
         try {
             if (isServiceConnected()) {
                 return getTelecomService().getActiveSubscription();
@@ -946,7 +946,7 @@ public class TelecomManager {
      * switches to other active subscription.
      * @hide
      */
-    public void switchToOtherActiveSub(long subId) {
+    public void switchToOtherActiveSub(int subId) {
         try {
             if (isServiceConnected()) {
                 getTelecomService().switchToOtherActiveSub(subId);
