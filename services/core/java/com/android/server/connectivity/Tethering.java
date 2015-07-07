4d4d3fc87c1b8ba10ba2229e036ebfac8bf7691a
zhaopb0311 <zhaopb0311@thundersoft.com>
2015-07-07 11:56:04 +0800

    FP2-448: Hotspot icon not show when hotspot trun on in the notification bar.
    FPII-448 [App_settings]  Hotspot icon not show when hotspot trun on in the notification bar, modify the logical of notification bar to show Hotspot icon.
    
    Change-Id: Ifaf9eed7246991154ce0923e2eb4319df1aa829a

--- services/core/java/com/android/server/connectivity/Tethering.java
+++ services/core/java/com/android/server/connectivity/Tethering.java
@@ -655,8 +655,8 @@ public class Tethering extends BaseNetworkObserver {
             if (bluetoothTethered) {
                 showTetheredNotification(com.android.internal.R.drawable.stat_sys_tether_general);
             } else {
-                /* We now have a status bar icon for WifiTethering, so drop the notification */
-                clearTetheredNotification();
+                /* Add the WifiTethering notification*/
+                showTetheredNotification(com.android.internal.R.drawable.stat_sys_tether_wifi);
             }
         } else if (bluetoothTethered) {
             showTetheredNotification(com.android.internal.R.drawable.stat_sys_tether_bluetooth);
