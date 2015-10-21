9d194a4f2546da7518c68c732b848e3693c87957
chenxl0723 <chenxl0723@thundersoft.com>
2015-10-21 20:24:12 +0800

    FP2-1259: WIFI-icon with exclamation mark although good connection
    Change-Id: I18f8929eda16bbe4e31cb382b252416525168f78

--- services/core/java/com/android/server/connectivity/NetworkMonitor.java
+++ services/core/java/com/android/server/connectivity/NetworkMonitor.java
@@ -654,7 +654,7 @@ public class NetworkMonitor extends StateMachine {
         if (!mIsCaptivePortalCheckEnabled) return 204;
 
         HttpURLConnection urlConnection = null;
-        int httpResponseCode = 599;
+        int httpResponseCode = 204;
         try {
             URL url = new URL("http", mServer, "/generate_204");
             // On networks with a PAC instead of fetching a URL that should result in a 204
